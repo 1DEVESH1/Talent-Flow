5ad10c708523bb923b935328616d87d08da12846
Devesh <136490739+1DEVESH1@users.noreply.github.com>
2025-10-04 22:40:35 +0530

    configure vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,56 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import { worker } from "./mocks/browser.js";
+// import { seedDatabase } from "./database/seed.js";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { BrowserRouter } from "react-router-dom";
+
+// // Create a client for React Query
+// const queryClient = new QueryClient();
+
+// // We wrap this in an async function to use await
+// async function prepareApp() {
+//   // We only want to run the mock server in development
+//   if (process.env.NODE_ENV === "development") {
+//     await seedDatabase();
+//     // The start method is async and returns a promise
+//     return worker.start({ onUnhandledRequest: "bypass" });
+//   }
+//   return Promise.resolve();
+// }
+
+// // Start the app after the mock server is ready
+// prepareApp().then(() => {
+//   createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//       <QueryClientProvider client={queryClient}>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </QueryClientProvider>
+//     </React.StrictMode>
+//   );
+// });
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { worker } from "./mocks/browser.js";
-import { seedDatabase } from "./database/seed.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 
-// Create a client for React Query
 const queryClient = new QueryClient();
-
-// We wrap this in an async function to use await
 async function prepareApp() {
-  // We only want to run the mock server in development
-  if (process.env.NODE_ENV === "development") {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./mocks/browser.js");
+    const { seedDatabase } = await import("./database/seed.js");
     await seedDatabase();
-    // The start method is async and returns a promise
     return worker.start({ onUnhandledRequest: "bypass" });
   }
   return Promise.resolve();
 }
 
-// Start the app after the mock server is ready
 prepareApp().then(() => {
   createRoot(document.getElementById("root")).render(
     <React.StrictMode>
@@ -33,3 +62,4 @@ prepareApp().then(() => {
     </React.StrictMode>
   );
 });
+
